988a78441ce0774677bb59053c05574b6ce4b452
Dotse Raphael <raphaelgraceman@gmail.com>
2025-12-09 23:40:54 +0000

    Renamed smoke test file

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -9,13 +9,10 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    options = Options()
-    options.add_argument("--headless=new")
-    self.driver = webdriver.Chrome(options=options)
+    self.driver = webdriver.Firefox()
     self.vars = {}
   
   def teardown_method(self, method):
